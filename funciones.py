6b4e48ded2f567774fb85d698bbe681ea5f8bf8e
Spdrd <juandiegoreyesrodriguez@gmail.com>
2024-08-12 21:14:52 -0500

    Reporte de inasistencias
    -Añadido el reporte de inasistencias
    -Solucionado error en las fechas
    -Protección a la hora de ingresar elementos no numéricos en los menus

--- funciones.py
+++ funciones.py
@@ -34,10 +34,12 @@ def leer_csv(guias):
 
 
 def agregar_guia(guias):
-  while True:
+  var_control = True
+  while var_control:
     print("Ingrese id [-1 para salir]")
     id = int(input())
     if id == -1:
+      var_control = False 
       break
     print("Ingrese nombre")
     nombre = input()
@@ -51,16 +53,29 @@ def agregar_guia(guias):
     guias.append(guia)
 
 def tomar_asistencia(guias):
-  print("Escoger fecha")
+  print("Escoger fecha [-1 para salir]")
   for fecha in ig.fechas:
     print(str(ig.fechas.index(fecha)) + ") " + fecha.strftime("%d-%m-%Y"))
+  var_control = True
+  while var_control:
+    seleccion_fecha = input()
+    if seleccion_fecha == "-1":
+      var_control = False
+      break
+    if seleccion_fecha.isnumeric() and int(seleccion_fecha) >= 0 and int(seleccion_fecha) < len(ig.fechas):
+      seleccion_fecha = int(seleccion_fecha)
+      break
+    else:
+      print("fecha no valida, intenta otra vez")
 
-  seleccion_fecha: int = int(input())
-
-  while True:
+  while var_control:
     print("Ingrese id de guia presente [-1 para salir]")
-    guia_presente: int = int(input())
+    guia_presente = input()
+    if guia_presente.isnumeric() or guia_presente == "-1":
+        guia_presente = int(guia_presente)
+
     if guia_presente == -1:
+      var_control = False 
       break
     for guia in guias:
       if guia.id == guia_presente:
@@ -68,29 +83,98 @@ def tomar_asistencia(guias):
         break
 
 def marcar_inasistencias(guias):
-  print("Escoger fecha")
+  print("Escoger fecha [-1 para salir]")
   for fecha in ig.fechas:
     print(str(ig.fechas.index(fecha)) + ") " + fecha.strftime("%d-%m-%Y"))
 
-  seleccion_fecha: int = int(input())
-  for guia in guias:
-    if guia.asistencias[seleccion_fecha] != ig.Cumplimiento.CUMPLIDO:
-      guia.asistencias[seleccion_fecha] = ig.Cumplimiento.NO_CUMPLIDO
+  var_control = True
+  while var_control:
+    seleccion_fecha = input()
+    if seleccion_fecha == "-1":
+      var_control = False
+      break
+    if seleccion_fecha.isnumeric() and int(seleccion_fecha) >= 0 and int(seleccion_fecha) < len(ig.fechas):
+      seleccion_fecha = int(seleccion_fecha)
+      break
+    else:
+      print("fecha no valida, intenta otra vez")
+  if var_control:
+    for guia in guias:
+      if guia.asistencias[seleccion_fecha] != ig.Cumplimiento.CUMPLIDO:
+        guia.asistencias[seleccion_fecha] = ig.Cumplimiento.NO_CUMPLIDO
 
 def imprimir_datos(guias):
   for guia in guias:
     print(guia)
 
+def buscar_guia(guias):
+  var_control = True
+  while var_control:
+    print("Ingrese el nombre o id del guia a buscar [-1 para salir]")
+    entrada = input()
+    if entrada == "-1":
+        var_control = False 
+        break
+    if entrada.isnumeric():
+      for guia in guias:
+        if guia.id == int(entrada):
+          guia_encontrado = True
+          print(f"Guia {guia.nombre} encontrado")
+          return guia
+    else:
+      guias_encontrados = []
+      for guia in guias:
+        if entrada in guia.nombre:
+          guia_encontrado = True
+          guias_encontrados.append(guia)
+      if len(guias_encontrados) > 0:
+        print("Escoger Guia")
+        for guia in guias_encontrados:
+          print(f"{guias_encontrados.index(guia)}) {guia.id} {guia.nombre}")
+        seleccion = input()
+        if seleccion.isnumeric():
+          seleccion = int(seleccion)
+          if seleccion >= 0 and seleccion < len(guias_encontrados):
+            return guias_encontrados[seleccion]
+    print("Guia no encontrado")
+  return None
+
+def inspeccionar_guia(guias):
+  var_control = True
+  while var_control:
+    guia = buscar_guia(guias)
+    if not (guia is None):
+      print("1) Validar porcentaje de asistencias")
+      print("-1) Salir")
+      entrada = input()
+      if entrada.isnumeric():
+        entrada = int(entrada)
+      if entrada == 1:
+        print(guia.reporte_asistencia())
+      elif entrada == -1:
+        var_control = False 
+        break
+    else:
+      var_control = False
+
+        
+
+
 def menu(guias):
-  while True:
+  var_control = True
+  while var_control:
     print("1) Leer Archivo")
     print("2) Guardar Archivo")
     print("3) Agregar Guia")
     print("4) Tomar Asistencia")
     print("5) Marcar Inasistencias")
     print("6) Imprimir Datos")
-    print("9) Salir")
-    seleccion = int(input())
+    print("7) Inspeccionar Guia")
+    print("-1) Salir")
+    seleccion = input()
+    if seleccion.isnumeric() or seleccion == "-1":
+        seleccion = int(seleccion)
+
     if seleccion == 1:
       leer_csv(guias)
     elif seleccion == 2:
@@ -103,7 +187,9 @@ def menu(guias):
       marcar_inasistencias(guias)
     elif seleccion == 6:
       imprimir_datos(guias)
-    elif seleccion == 9:
+    elif seleccion == 7:
+      inspeccionar_guia(guias)
+    elif seleccion == -1:
       break
     else:
       print("Selección inválida")
--- info_guias.py
+++ info_guias.py
@@ -23,8 +23,7 @@ fechas = [
     dt.datetime(day=5, month=10, year=2024),
     dt.datetime(day=12, month=10, year=2024),
     dt.datetime(day=19, month=10, year=2024),
-    dt.datetime(day=26, month=10, year=2024),
-    dt.datetime(day=5, month=10, year=2024),
+    dt.datetime(day=26, month=10, year=2024)
 ]
 
 
@@ -46,3 +45,23 @@ class Guia:
 
   def __repr__(self):
     return f"{self.id}, {self.nombre}, {self.lider}, {self.edad}, {self.telefono},{self.asistencias}"
+
+  def reporte_asistencia(self):
+    num_cumplidas = 0
+    num_no_cumplidas = 0
+    num_pendientes = 0
+
+    for asistencia in self.asistencias:
+      if asistencia == Cumplimiento.CUMPLIDO:
+        num_cumplidas += 1
+      elif asistencia == Cumplimiento.NO_CUMPLIDO:
+        num_no_cumplidas += 1
+      elif asistencia == Cumplimiento.PENDIENTE:
+        num_pendientes += 1
+    
+    total_sesiones = num_pendientes + num_cumplidas + num_no_cumplidas
+    porcentaje_fallas = (num_no_cumplidas / total_sesiones) * 100
+
+    return f"Asistencias: {num_cumplidas}; Fallas: {num_no_cumplidas}; Pendientes: {num_pendientes}; Total: {total_sesiones}; %fallas: {porcentaje_fallas};"
+    
+
